962681e804f0fa2dffb8834bca21af8b3a7367b7
Dawit Getachew <dawit.getachew@Kifiya.et>
2025-07-14 14:52:46 +0300

    typoupdate

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
-import * as cookieParser from 'cookie-parser';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
