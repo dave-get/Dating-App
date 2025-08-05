76ce7f96ec4d526914830eeb733505d7ac41aefc
Dawit Getachew <dawit.getachew@Kifiya.et>
2025-08-06 00:12:39 +0300

    cookie update

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
       cookie: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+        sameSite: 'none',
         maxAge: 1000 * 60 * 60 * 24, // 24 hours
       },
     }),
